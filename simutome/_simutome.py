2f80ba29c687513b5a65e84d4d00b3db7d70caf6
Jonas Windhager <jonas.windhager@uzh.ch>
2022-05-02 19:59:39 +0200

    cell swapping

--- simutome/_simutome.py
+++ simutome/_simutome.py
@@ -6,7 +6,7 @@ from scipy.stats import truncnorm
 from skimage.transform import AffineTransform
 
 
-Section = tuple[np.ndarray, np.ndarray, Optional[np.ndarray]]
+Section = tuple[np.ndarray, np.ndarray, np.ndarray, Optional[np.ndarray]]
 
 
 class Simutome:
@@ -18,6 +18,7 @@ class Simutome:
         image_shear: float = 0.0,
         image_translation: tuple[float, float] = (0.0, 0.0),
         exclude_cells: bool = False,
+        exclude_cells_swap: float = 0.0,
         section_thickness: Optional[float] = None,
         cell_diameter_mean: Optional[float] = None,
         cell_diameter_std: Optional[float] = None,
@@ -27,7 +28,6 @@ class Simutome:
         cell_division_probab: float = 0.0,
         cell_division_dist_mean: Optional[float] = None,
         cell_division_dist_std: Optional[float] = None,
-        cell_swapping_probab: float = 0.0,
         shuffle_cells: bool = True,
         seed=None,
     ) -> None:
@@ -35,6 +35,8 @@ class Simutome:
             raise ValueError("image_occlusion")
         if image_scale[0] <= 0.0 or image_scale[1] <= 0.0:
             raise ValueError("image_scale")
+        if exclude_cells_swap < 0.0 or exclude_cells_swap > 1.0:
+            raise ValueError("exclude_cells_swap")
         if exclude_cells and (section_thickness is None or section_thickness <= 0.0):
             raise ValueError("section_thickness")
         if exclude_cells and (cell_diameter_mean is None or cell_diameter_mean <= 0.0):
@@ -59,14 +61,13 @@ class Simutome:
             cell_division_dist_std is None or cell_division_dist_std <= 0.0
         ):
             raise ValueError("cell_division_dist_std")
-        if cell_swapping_probab < 0.0 or cell_swapping_probab > 1.0:
-            raise ValueError("cell_swapping_probab")
         self.image_occlusion = image_occlusion
         self.image_scale = image_scale
         self.image_rotation = image_rotation
         self.image_shear = image_shear
         self.image_translation = image_translation
         self.exclude_cells = exclude_cells
+        self.exclude_cells_swap = exclude_cells_swap
         self.section_thickness = section_thickness
         self.cell_diameter_mean = cell_diameter_mean
         self.cell_diameter_std = cell_diameter_std
@@ -76,7 +77,6 @@ class Simutome:
         self.cell_division_probab = cell_division_probab
         self.cell_division_dist_mean = cell_division_dist_mean
         self.cell_division_dist_std = cell_division_dist_std
-        self.cell_swapping_probab = cell_swapping_probab
         self.shuffle_cells = shuffle_cells
         self._seed_rng = np.random.default_rng(seed=seed)
 
@@ -177,51 +177,63 @@ class Simutome:
         ):
             raise ValueError("cell_clusters")
         section_rng = np.random.default_rng(seed=self._seed_rng.integers(sys.maxsize))
+        cell_points = cell_points.copy()
+        cell_intensities = None
+        if cell_intensities is not None:
+            cell_intensities = cell_intensities.copy()
+        cell_clusters = None
+        if cell_clusters is not None:
+            cell_clusters = cell_clusters.copy()
         cell_indices = np.arange(len(cell_points))
+        new_cell_mask = np.zeros(len(cell_points), dtype=bool)
         if self.image_occlusion > 0.0:
             if image_size is None:
                 raise ValueError("image_size")
             image_occlusion_mask = self._occlude_image(cell_points, image_size)
+            cell_indices = cell_indices[~image_occlusion_mask]
+            new_cell_mask = new_cell_mask[~image_occlusion_mask]
             cell_points = cell_points[~image_occlusion_mask]
             if cell_intensities is not None:
                 cell_intensities = cell_intensities[~image_occlusion_mask]
             if cell_clusters is not None:
                 cell_clusters = cell_clusters[~image_occlusion_mask]
-            cell_indices = cell_indices[~image_occlusion_mask]
         if self.exclude_cells:
-            cell_exclusion_mask = self._exclude_cells(
+            cell_exclusion_mask, cell_swapping_mask = self._exclude_cells(
                 len(cell_points), None, section_rng
             )
+            if cell_intensities is not None:
+                for i in np.nonzero(cell_swapping_mask)[0]:
+                    m = cell_clusters == cell_clusters[i]
+                    j = section_rng.choice(len(cell_indices), p=m / np.sum(m))
+                    new_cell_mask[i] = new_cell_mask[j] = True
+                    cell_intensities[i], cell_intensities[j] = (
+                        cell_intensities[j],
+                        cell_intensities[i],
+                    )
+            cell_indices = cell_indices[~cell_exclusion_mask]
+            new_cell_mask = new_cell_mask[~cell_exclusion_mask]
             cell_points = cell_points[~cell_exclusion_mask]
             if cell_intensities is not None:
                 cell_intensities = cell_intensities[~cell_exclusion_mask]
             if cell_clusters is not None:
                 cell_clusters = cell_clusters[~cell_exclusion_mask]
-            cell_indices = cell_indices[~cell_exclusion_mask]
         if self.displace_cells:
             cell_points += section_rng.multivariate_normal(
                 np.ones(2) * self.cell_displacement_mean,
                 np.eye(2) * self.cell_displacement_var,
                 size=len(cell_points),
             )
-        if self.cell_swapping_probab > 0.0:
-            if cell_intensities is None:
-                raise ValueError("cell_intensities")
-            if cell_clusters is None:
-                raise ValueError("cell_clusters")
-            swapped_cell_order = self._swap_cells(
-                cell_indices, None, cell_clusters, None, section_rng
-            )
-            cell_intensities = cell_intensities[swapped_cell_order]
         if self.cell_division_probab > 0.0:
-            divided_cell_order, cell_points = self._divide_cells(
+            divided_cell_order, new_cell_points = self._divide_cells(
                 cell_indices, None, cell_points, None, section_rng
             )
+            cell_indices = cell_indices[divided_cell_order]
+            new_cell_mask = new_cell_mask[divided_cell_order]
+            cell_points = new_cell_points
             if cell_intensities is not None:
                 cell_intensities = cell_intensities[divided_cell_order]
             if cell_clusters is not None:
                 cell_clusters = cell_clusters[divided_cell_order]
-            cell_indices = cell_indices[divided_cell_order]
         if (
             self.image_scale != (1.0, 1.0)
             or self.image_rotation != 0.0
@@ -231,13 +243,14 @@ class Simutome:
             cell_points = self._transform_image(cell_points)
         if self.shuffle_cells:
             shuffled_cell_order = section_rng.permutation(len(cell_points))
+            cell_indices = cell_indices[shuffled_cell_order]
+            new_cell_mask = new_cell_mask[shuffled_cell_order]
             cell_points = cell_points[shuffled_cell_order]
             if cell_intensities is not None:
                 cell_intensities = cell_intensities[shuffled_cell_order]
             if cell_clusters is not None:
                 cell_clusters = cell_clusters[shuffled_cell_order]
-            cell_indices = cell_indices[shuffled_cell_order]
-        return cell_indices, cell_points, cell_intensities
+        return cell_indices, new_cell_mask, cell_points, cell_intensities
 
     def generate_section_pair(
         self,
@@ -274,6 +287,8 @@ class Simutome:
             cell_clusters2 = cell_clusters.copy()
         cell_indices1 = np.arange(len(cell_points1))
         cell_indices2 = np.arange(len(cell_points2))
+        new_cell_mask1 = np.zeros(len(cell_points1), dtype=bool)
+        new_cell_mask2 = np.zeros(len(cell_points2), dtype=bool)
         if self.image_occlusion > 0.0:
             if image_size is None:
                 raise ValueError("image_size")
@@ -283,6 +298,10 @@ class Simutome:
             image_occlusion_mask2 = self._occlude_image(
                 cell_points2, image_size, where="right"
             )
+            cell_indices1 = cell_indices1[~image_occlusion_mask1]
+            cell_indices2 = cell_indices2[~image_occlusion_mask2]
+            new_cell_mask1 = new_cell_mask1[~image_occlusion_mask1]
+            new_cell_mask2 = new_cell_mask2[~image_occlusion_mask2]
             cell_points1 = cell_points1[~image_occlusion_mask1]
             cell_points2 = cell_points2[~image_occlusion_mask2]
             if cell_intensities1 is not None:
@@ -293,12 +312,35 @@ class Simutome:
                 cell_clusters1 = cell_clusters1[~image_occlusion_mask1]
             if cell_clusters2 is not None:
                 cell_clusters2 = cell_clusters2[~image_occlusion_mask2]
-            cell_indices1 = cell_indices1[~image_occlusion_mask1]
-            cell_indices2 = cell_indices2[~image_occlusion_mask2]
         if self.exclude_cells:
-            cell_exclusion_mask1, cell_exclusion_mask2 = self._exclude_cells(
-                cell_indices1, cell_indices2, section_rng
-            )
+            (
+                cell_exclusion_mask1,
+                cell_exclusion_mask2,
+                cell_swapping_mask1,
+                cell_swapping_mask2,
+            ) = self._exclude_cells(cell_indices1, cell_indices2, section_rng)
+            if cell_intensities1 is not None:
+                for i in np.flatnonzero(cell_swapping_mask1):
+                    m = cell_clusters1 == cell_clusters1[i]
+                    j = section_rng.choice(len(cell_indices1), p=m / np.sum(m))
+                    new_cell_mask1[i] = new_cell_mask1[j] = True
+                    cell_intensities1[i], cell_intensities1[j] = (
+                        cell_intensities1[j],
+                        cell_intensities1[i],
+                    )
+            if cell_intensities2 is not None:
+                for i in np.flatnonzero(cell_swapping_mask2):
+                    m = cell_clusters2 == cell_clusters2[i]
+                    j = section_rng.choice(len(cell_indices2), p=m / np.sum(m))
+                    new_cell_mask2[i] = new_cell_mask2[j] = True
+                    cell_intensities2[i], cell_intensities2[j] = (
+                        cell_intensities2[j],
+                        cell_intensities2[i],
+                    )
+            cell_indices1 = cell_indices1[~cell_exclusion_mask1]
+            cell_indices2 = cell_indices2[~cell_exclusion_mask2]
+            new_cell_mask1 = new_cell_mask1[~cell_exclusion_mask1]
+            new_cell_mask2 = new_cell_mask2[~cell_exclusion_mask2]
             cell_points1 = cell_points1[~cell_exclusion_mask1]
             cell_points2 = cell_points2[~cell_exclusion_mask2]
             if cell_intensities1 is not None:
@@ -309,8 +351,6 @@ class Simutome:
                 cell_clusters1 = cell_clusters1[~cell_exclusion_mask1]
             if cell_clusters2 is not None:
                 cell_clusters2 = cell_clusters2[~cell_exclusion_mask2]
-            cell_indices1 = cell_indices1[~cell_exclusion_mask1]
-            cell_indices2 = cell_indices2[~cell_exclusion_mask2]
         if self.displace_cells:
             displacement = section_rng.multivariate_normal(
                 0.5 * np.ones(2) * self.cell_displacement_mean,
@@ -319,20 +359,6 @@ class Simutome:
             )
             cell_points1[np.isin(cell_indices1, cell_indices2), :] += displacement
             cell_points2[np.isin(cell_indices2, cell_indices1), :] -= displacement
-        if self.cell_swapping_probab > 0.0:
-            if cell_intensities is None:
-                raise ValueError("cell_intensities")
-            if cell_clusters is None:
-                raise ValueError("cell_clusters")
-            swapped_cell_order1, swapped_cell_order2 = self._swap_cells(
-                cell_indices1,
-                cell_indices2,
-                cell_clusters1,
-                cell_clusters2,
-                section_rng,
-            )
-            cell_intensities1 = cell_intensities1[swapped_cell_order1]
-            cell_intensities2 = cell_intensities2[swapped_cell_order2]
         if self.cell_division_probab > 0.0:
             (
                 divided_cell_order1,
@@ -340,8 +366,16 @@ class Simutome:
                 cell_points1,
                 cell_points2,
             ) = self._divide_cells(
-                cell_indices1, cell_indices2, cell_points1, cell_points2, section_rng
+                cell_indices1,
+                cell_indices2,
+                cell_points1,
+                cell_points2,
+                section_rng,
             )
+            cell_indices1 = cell_indices1[divided_cell_order1]
+            cell_indices2 = cell_indices2[divided_cell_order2]
+            new_cell_mask1 = new_cell_mask1[divided_cell_order1]
+            new_cell_mask2 = new_cell_mask2[divided_cell_order2]
             if cell_intensities1 is not None:
                 cell_intensities1 = cell_intensities1[divided_cell_order1]
             if cell_intensities2 is not None:
@@ -350,8 +384,6 @@ class Simutome:
                 cell_clusters1 = cell_clusters1[divided_cell_order1]
             if cell_clusters2 is not None:
                 cell_clusters2 = cell_clusters2[divided_cell_order2]
-            cell_indices1 = cell_indices1[divided_cell_order1]
-            cell_indices2 = cell_indices2[divided_cell_order2]
         if (
             self.image_scale != (1.0, 1.0)
             or self.image_rotation != 0.0
@@ -363,6 +395,10 @@ class Simutome:
         if self.shuffle_cells:
             shuffled_cell_order1 = section_rng.permutation(len(cell_points1))
             shuffled_cell_order2 = section_rng.permutation(len(cell_points2))
+            cell_indices1 = cell_indices1[shuffled_cell_order1]
+            cell_indices2 = cell_indices2[shuffled_cell_order2]
+            new_cell_mask1 = new_cell_mask1[shuffled_cell_order1]
+            new_cell_mask2 = new_cell_mask2[shuffled_cell_order2]
             cell_points1 = cell_points1[shuffled_cell_order1]
             cell_points2 = cell_points2[shuffled_cell_order2]
             if cell_intensities1 is not None:
@@ -373,11 +409,9 @@ class Simutome:
                 cell_clusters1 = cell_clusters1[shuffled_cell_order1]
             if cell_clusters2 is not None:
                 cell_clusters2 = cell_clusters2[shuffled_cell_order2]
-            cell_indices1 = cell_indices1[shuffled_cell_order1]
-            cell_indices2 = cell_indices2[shuffled_cell_order2]
         return (
-            (cell_indices1, cell_points1, cell_intensities1),
-            (cell_indices2, cell_points2, cell_intensities2),
+            (cell_indices1, new_cell_mask1, cell_points1, cell_intensities1),
+            (cell_indices2, new_cell_mask2, cell_points2, cell_intensities2),
         )
 
     def _occlude_image(
@@ -399,7 +433,10 @@ class Simutome:
         cell_indices1: np.ndarray,
         cell_indices2: Optional[np.ndarray],
         section_rng: np.random.Generator,
-    ) -> Union[np.ndarray, tuple[np.ndarray, np.ndarray]]:
+    ) -> Union[
+        tuple[np.ndarray, np.ndarray],
+        tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray],
+    ]:
         cell_indices = cell_indices1
         if cell_indices2 is not None:
             cell_indices = np.union1d(cell_indices1, cell_indices2)
@@ -412,64 +449,39 @@ class Simutome:
             random_state=section_rng,
         )
         s = np.ceil(d / self.section_thickness)
-        cell_exclusion_mask = section_rng.random(size=len(cell_indices)) < 1.0 / s
+        selected_cell_indices = cell_indices[
+            section_rng.random(size=len(cell_indices)) < 1.0 / s
+        ]
+        n_exclude = int(len(selected_cell_indices) * (1 - self.exclude_cells_swap))
+        n_swap = int(len(selected_cell_indices) * self.exclude_cells_swap / 2)
+        excluded_cell_indices = selected_cell_indices[:n_exclude]
+        swapped_cell_indices = selected_cell_indices[n_exclude : n_exclude + n_swap]
         if cell_indices2 is not None:
             excluded_cell_indices1, excluded_cell_indices2 = self._split_cell_indices(
-                cell_indices[cell_exclusion_mask],
+                excluded_cell_indices,
+                cell_indices1,
+                cell_indices2,
+                section_rng,
+            )
+            swapped_cell_indices1, swapped_cell_indices2 = self._split_cell_indices(
+                swapped_cell_indices,
                 cell_indices1,
                 cell_indices2,
                 section_rng,
             )
             cell_exclusion_mask1 = np.isin(cell_indices1, excluded_cell_indices1)
             cell_exclusion_mask2 = np.isin(cell_indices2, excluded_cell_indices2)
-            return cell_exclusion_mask1, cell_exclusion_mask2
-        return cell_exclusion_mask
-
-    def _swap_cells(
-        self,
-        cell_indices1: np.ndarray,
-        cell_indices2: Optional[np.ndarray],
-        cell_clusters1: np.ndarray,
-        cell_clusters2: Optional[np.ndarray],
-        section_rng: np.random.Generator,
-    ) -> Union[np.ndarray, tuple[np.ndarray, np.ndarray]]:
-        cell_indices = cell_indices1
-        cell_clusters = cell_clusters1
-        if cell_indices2 is not None and cell_clusters2 is not None:
-            cell_indices = np.union1d(cell_indices1, cell_indices2)
-            cell_clusters = None
-        n = section_rng.binomial(len(cell_indices), p=self.cell_swapping_probab)
-        swapped_cell_indices = section_rng.choice(cell_indices, size=n, replace=False)
-        if cell_indices2 is not None and cell_clusters2 is not None:
-            swapped_cell_indices1, swapped_cell_indices2 = self._split_cell_indices(
-                swapped_cell_indices, cell_indices1, cell_indices2, section_rng
+            cell_swapping_mask1 = np.isin(cell_indices1, swapped_cell_indices1)
+            cell_swapping_mask2 = np.isin(cell_indices2, swapped_cell_indices2)
+            return (
+                cell_exclusion_mask1,
+                cell_exclusion_mask2,
+                cell_swapping_mask1,
+                cell_swapping_mask2,
             )
-            swapped_cell_order1 = np.arange(len(cell_indices1))
-            swapped_cell_order2 = np.arange(len(cell_indices2))
-            for i in swapped_cell_indices1:
-                m = cell_clusters1 == cell_clusters1[i]
-                j = section_rng.choice(len(cell_indices1), p=m / np.sum(m))
-                swapped_cell_order1[i], swapped_cell_order1[j] = (
-                    swapped_cell_order1[j],
-                    swapped_cell_order1[i],
-                )
-            for i in swapped_cell_indices2:
-                m = cell_clusters2 == cell_clusters2[i]
-                j = section_rng.choice(len(cell_indices2), p=m / np.sum(m))
-                swapped_cell_order2[i], swapped_cell_order2[j] = (
-                    swapped_cell_order2[j],
-                    swapped_cell_order2[i],
-                )
-            return swapped_cell_order1, swapped_cell_order2
-        swapped_cell_order = np.arange(len(cell_indices))
-        for i in swapped_cell_indices:
-            m = cell_clusters == cell_clusters[i]
-            j = section_rng.choice(len(cell_indices), p=m / np.sum(m))
-            swapped_cell_order[i], swapped_cell_order[j] = (
-                swapped_cell_order[j],
-                swapped_cell_order[i],
-            )
-        return swapped_cell_order
+        cell_exclusion_mask = np.isin(cell_indices, excluded_cell_indices)
+        cell_swapping_mask = np.isin(cell_indices, swapped_cell_indices)
+        return cell_exclusion_mask, cell_swapping_mask
 
     def _divide_cells(
         self,
